7675077d9f23cc1f7be553b793d6c08f8cd71a65
veysiulger <veysiulger@gmail.com>
2023-04-29 19:48:24 +0300

    created classes

---
+++ Level.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math_app
+{
+    public class Level
+    {
+        public byte level;
+        public byte levelScore;
+        public byte pointPerQuestion;
+
+
+        public Level(byte level, byte score, byte point)
+        {
+            this.level = level;
+            this.levelScore = score;
+            this.pointPerQuestion = point;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math_app
+{
+    public class User
+    {
+        public string userName;
+        public int userScore;
+        public int userHighestScore;
+        public byte userLevel;
+        public byte userAnswerCount;
+
+        public User(string name, int score, int highestScore, byte level, byte answerCount)
+        {
+            this.userName = name;
+            this.userScore = score;
+            this.userHighestScore = highestScore;
+            this.userLevel = level;
+            this.userAnswerCount = answerCount;
+
+        }
+
+        public void showUserInfos()
+        {
+            Console.WriteLine("\n****************************");
+            Console.WriteLine($"Isim: {userName} \nPuan:{userScore} \nSeviye:{userLevel} \nRekor:{userHighestScore} \nCevap Hakkı:{userAnswerCount}");
+            Console.WriteLine("\n****************************");
+        }
+    }
+}
